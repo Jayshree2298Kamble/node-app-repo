fa08fe928902b6c456500f51c173e2b554a83e97
Jayshree2298Kamble <jayshree.2298@gmail.com>
2024-09-17 09:34:11 -0700

    update
    update

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-console.log("my first app");
-console.log(global);
-console.log(console);
-console.log(process);
-console.log(__filename);
-console.log(__dirname);
-console.log(module);
+// console.log("my first app");
+// console.log(global);
+// console.log(console);
+// console.log(process);
+// console.log(__filename);
+// console.log(__dirname);
+// console.log(module);
 
-// const os = require('os');
-// console.log(os.arch());
-// console.log(os.cpus());
-// console.log(os.freemem());
-// console.log(os.totalmem());
-// console.log(os.hostname());
-// console.log(os.platform());
-// console.log(os.release());
-// console.log(os.type());
-// console.log(os.uptime());
-// console.log(os.userInfo());
+const os = require('os');
+console.log(os.arch());
+console.log(os.cpus());
+console.log(os.freemem());
+console.log(os.totalmem());
+console.log(os.hostname());
+console.log(os.platform());
+console.log(os.release());
+console.log(os.type());
+console.log(os.uptime());
+console.log(os.userInfo());
 
 
 
